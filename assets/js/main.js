e346d64bf5240b010bde0c25af2b0b236d8fcd7e
aguslu0 <artifexted@gmail.com>
2022-06-21 19:17:56 -0300

    Agregué comentarios y realicé cambios en la función

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 let iProductSelected = NaN;
 
+/* Creamos un while para ejecutar un prompt hasta que elija una de las opciones que queremos. 
+Lo hacemos con DO para que se ejecute por lo menos una vez.*/
 do {
     iProductSelected = parseInt(prompt("Elige las 8 de oro que quisieras comprar: \n1. Azucaradas \n2. Clásicas \n3. Agridulces \n\n0. Salir del menú \nDEBES INGRESAR UN NUMERO."));
 
@@ -15,43 +17,28 @@ do {
             continue;
     }
 } while (iProductSelected > 3 || isNaN(iProductSelected) || iProductSelected < 0);
+// WHILE: En caso de que el numero ingresado sea mayor a 3, no sea un numero o sea menor a 0 (negativo) continua el bucle.
 
+// Creamos una función en la cual ejecutamos acciones en caso de que haya elegido algún producto.
 function product(id) {
     let fProductPrice;
+    let sProductOption;
 
-    switch(id) {
-        case 1:
-            fProductPrice = parseFloat(prompt("Ingresa el costo de las 8 de oro azucaradas"));
+    // Damos nombre a la opción seleccionada en una variable.
+    if(id == 1) { sProductOption = "azucaradas"; }
+    else if (id == 2) { sProductOption = "clasicas"; }
+    else if(id == 3) { sProductOption = "agridulces"; }
+    else { alert("ERROR: Ocurrió un error inesperado con tu selección."); }
+    // El último else es solamente por si ocurre algún bug y el id da otro valor fuera de los que debería.
 
-            if(isNaN(fProductPrice) || fProductPrice <= 0) {
-                alert("ERROR: Ingresa un número. Debe ser mayor a 0")
-                product(id);
-                break;
-            } else {
-                alert("Elegiste 8 de oro azucaradas a un valor de: $" + fProductPrice);
-                break;
-            }
-        case 2:
-            fProductPrice = parseFloat(prompt("Ingresa el costo de las 8 de oro clásicas"));
+    // Pedimos al usuario que ingrese un precio al producto mediante prompt...
+    fProductPrice = parseFloat(prompt("Ingresa el costo de las 8 de oro " + sProductOption));
 
-            if(isNaN(fProductPrice) || fProductPrice <= 0) {
-                alert("ERROR: Ingresa un número. Debe ser mayor a 0")
-                product(id);
-                break;
-            } else {
-                alert("Elegiste 8 de oro azucaradas a un valor de: $" + fProductPrice);
-                break;
-            }
-        case 3:
-            fProductPrice = parseFloat(prompt("Ingresa el costo de las 8 de oro agridulces"));
-
-            if(isNaN(fProductPrice) || fProductPrice <= 0) {
-                alert("ERROR: Ingresa un número. Debe ser mayor a 0")
-                product(id);
-                break;
-            } else {
-                alert("Elegiste 8 de oro azucaradas a un valor de: $" + fProductPrice);
-                break;
-            }
+    // Hacemos una verificación de lo ingresado por el usuario
+    if(isNaN(fProductPrice) || fProductPrice <= 0) {
+        alert("ERROR: Ingresa un número. Debe ser mayor a 0")
+        product(id);
+    } else {
+        alert("Elegiste 8 de oro azucaradas a un valor de: $" + fProductPrice);
     }
 }
