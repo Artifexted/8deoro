bceed9d5b67d9643ed736e29fc4cc73ebabc0e52
aguslu0 <artifexted@gmail.com>
2022-06-25 00:41:04 -0300

    Adaptamos el proyecto con arrays

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,64 @@
+/* 
+    Acostumbro a declarar las variables con inicial de la siguiente manera (y luego empleo camelCase):
+        i = int
+        f = float
+        s = string
+        a = arrays
+*/
+
 let iProductSelected = NaN;
+let iTotal = 0;
+let iContinue;
 
-/* Creamos un while para ejecutar un prompt hasta que elija una de las opciones que queremos. 
-Lo hacemos con DO para que se ejecute por lo menos una vez.*/
-do {
-    iProductSelected = parseInt(prompt("Elige las 8 de oro que quisieras comprar: \n1. Azucaradas \n2. Clásicas \n3. Agridulces \n\n0. Salir del menú \nDEBES INGRESAR UN NUMERO."));
+// Constantes en las cuales almacenamos los productos y precios del sitio
+const aOptions8DO = [ "azucaradas", "clasicas", "agridulces" ];
+const aPrice8DO = [ 110, 80, 90 ];
 
-    switch(iProductSelected) {
-        case 1: case 2: case 3:
-            product(iProductSelected);
+// Esta función se ejecutará al hacer click en el botón "Comprar" gracias al evento onClick
+// En esta función creamos el prompt con datos recibidos de un bucle FOR en el cual pasamos los datos de los arrays.
+function buy8deoro() {
+    let sOptions = "";
+    
+    for(let i = 0; i < aOptions8DO.length; i++) {
+        sOptions += aOptions8DO[i].toUpperCase() + " ($" + aPrice8DO[i] + ")\n";
+    }
+
+    const optSelected = prompt("Elige las 8 de oro que quisieras comprar:\n\n" + sOptions).toLowerCase();
+
+    if(aOptions8DO.includes(optSelected)) {
+        buyOptSelected(aOptions8DO.indexOf(optSelected));
+    } else if(optSelected != null) {
+        alert("ERROR: Ingresa una opción válida!");
+        buy8deoro();
+    }
+}
+
+// En caso de que el usuario ingrese una opción válida llamamos a esta función que le preguntará si desea seguir comprando y llamaremos a la función totalBuy con cada compra.
+function buyOptSelected(option) {
+    iContinue = parseInt(prompt("Elegiste las 8 de oro " + aOptions8DO[option].toUpperCase() + " por un precio de $" + aPrice8DO[option] + "\n¿Desea seguir comprando? \n\n1. Si \n2. No"));
+    totalBuy(aPrice8DO[option]);
+
+    switch(iContinue) {
+        case 1:
+            alert("Excelente! Hasta ahora vas gastando: $" + totalBuy());
+            buy8deoro();
             break;
-        case 0:
-            alert("Saliste exitosamente.");
+        case 2:
+            alert("¡Muchas gracias por tu compra! El total es: $" + totalBuy());
+            iTotal = 0;
             break;
         default:
-            alert("Ingresa una opción valida.");
-            continue;
+            alert("ERROR: No ingresaste una opción valida! Tu compra se ha cancelado.");
+            iTotal = 0;
+            break;
     }
-} while (iProductSelected > 3 || isNaN(iProductSelected) || iProductSelected < 0);
-// WHILE: En caso de que el numero ingresado sea mayor a 3, no sea un numero o sea menor a 0 (negativo) continua el bucle.
+}
 
-// Creamos una función en la cual ejecutamos acciones en caso de que haya elegido algún producto.
-function product(id) {
-    let fProductPrice;
-    let sProductOption;
+// En esta función vamos obteniendo el total de lo que lleva gastando el cliente utilizando la variable iTotal.
+function totalBuy(valor) {
+    if(isNaN(valor)) { valor = 0; }
 
-    // Damos nombre a la opción seleccionada en una variable.
-    if(id == 1) { sProductOption = "azucaradas"; }
-    else if (id == 2) { sProductOption = "clasicas"; }
-    else if(id == 3) { sProductOption = "agridulces"; }
-    else { alert("ERROR: Ocurrió un error inesperado con tu selección."); }
-    // El último else es solamente por si ocurre algún bug y el id da otro valor fuera de los que debería.
+    iTotal += valor;
 
-    // Pedimos al usuario que ingrese un precio al producto mediante prompt...
-    fProductPrice = parseFloat(prompt("Ingresa el costo de las 8 de oro " + sProductOption));
-
-    // Hacemos una verificación de lo ingresado por el usuario
-    if(isNaN(fProductPrice) || fProductPrice <= 0) {
-        alert("ERROR: Ingresa un número. Debe ser mayor a 0")
-        product(id);
-    } else {
-        alert("Elegiste 8 de oro azucaradas a un valor de: $" + fProductPrice);
-    }
+    return iTotal;
 }
