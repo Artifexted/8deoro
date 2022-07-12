28401e43442f7d792acacab8b263b2033535e4cd
aguslu0 <artifexted@gmail.com>
2022-07-11 21:19:29 -0300

    Prototipo de carrito

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,12 +6,8 @@
         a = arrays
 */
 
-let iProductSelected = NaN;
-let iTotal = 0;
-let iContinue;
-
 // Arrays en las cuales almacenamos los productos y precios del sitio
-const a8DO = [ 
+const aProducts = [ 
     {
         id: 1,
         title: "azucaradas",
@@ -35,93 +31,108 @@ const a8DO = [
     }
 ];
 
-const aOptions8DO = [ "azucaradas", "clasicas", "agridulces" ];
-const aPrice8DO = [ 110, 80, 90 ];
-
-let iAzucaradas = 0, iClasicas = 0, iAgridulces = 0;
-
-// Esta función se ejecutará al hacer click en el botón "Comprar" gracias al evento onClick
-// En esta función creamos el prompt con datos recibidos de un bucle FOR en el cual pasamos los datos de los arrays.
-function buy8deoro() {
-    let sOptions = "";
-    
-    for(let i = 0; i < aOptions8DO.length; i++) {
-        sOptions += aOptions8DO[i].toUpperCase() + " ($" + aPrice8DO[i] + ")\n";
-    }
-
-    let optSelected = prompt("Elige las 8 de oro que quisieras comprar:\n\n" + sOptions).toLowerCase();
-
-    if(aOptions8DO.includes(optSelected)) {
-        buyOptSelected(aOptions8DO.indexOf(optSelected));
-    } else if(optSelected != null) {
-        alert("ERROR: Ingresa una opción válida!");
-        buy8deoro();
-    }
-}
-
-// En caso de que el usuario ingrese una opción válida llamamos a esta función que le preguntará si desea seguir comprando y llamaremos a la función totalBuy con cada compra.
-function buyOptSelected(option) {
-    iContinue = parseInt(prompt("Elegiste las 8 de oro " + aOptions8DO[option].toUpperCase() + " por un precio de $" + aPrice8DO[option] + "\n¿Desea seguir comprando? \n\n1. Si \n2. No"));
-    totalBuy(aPrice8DO[option]);
-
-    switch(iContinue) {
-        case 1:
-            alert("Excelente! Hasta ahora vas gastando: $" + totalBuy());
-            cart(aOptions8DO[option].toLowerCase());
-            buy8deoro();
-            break;
-        case 2:
-            alert("¡Muchas gracias por tu compra! \nEl total es: $" + totalBuy());
-            cart(aOptions8DO[option].toLowerCase());
-            iTotal = 0;
-            break;
-        default:
-            alert("ERROR: No ingresaste una opción valida! Tu compra se ha cancelado.");
-            iTotal = 0;
-            break;
-    }
-}
-
-// En esta función vamos obteniendo el total de lo que lleva gastando el cliente utilizando la variable iTotal.
-function totalBuy(valor) {
-    if(isNaN(valor)) { valor = 0; }
-
-    iTotal += valor;
-
-    return iTotal;
-}
-
-// En esta función hacemos el carrito
-function cart(option) {
-    switch(option)
-    {
-        case "azucaradas":
-            iAzucaradas++;
-            break;
-        case "clasicas":
-            iClasicas++;
-            break;
-        case "agridulces":
-            iAgridulces++;
-            break;
-    }
-
-    return console.log("Hasta ahora tenés: " + iAzucaradas + " 8 de oro azucaradas\n" + iClasicas + " 8 de oro clasicas\n" + iAgridulces + " 8 de oro agridulces");
-}
-
 // Empezamos a interactuar con HTML
 let htmlCode = ``;
 
-a8DO.forEach(function(ochoDeOro) {
+aProducts.forEach((product, idProduct) => {
     htmlCode += `<article>
-    <img src="${ochoDeOro.img}" alt="${
-    ochoDeOro.alt
-  }">
-    <h3>8 de Oro ${ochoDeOro.title}</h3>
-    <p>Precio: $${ochoDeOro.price}</p>
+    <img src="${product.img}" alt="${product.alt}">
+    <h3>8 de Oro ${product.title}</h3>
+    <p>Precio: $${product.price}</p>
+    <button onClick="addToCart(${idProduct})">COMPRAR</button>
   </article>`;
 });
 
 const sCards = document.querySelector("#cards");
 
 sCards.innerHTML = htmlCode;
+
+// Comenzamos a crear el carrito
+let modalCart = document.getElementById("cart");
+let iTotal = 0;
+
+const drawCart = () => {
+    modalCart.className = "cart";
+    modalCart.innerHTML = "";
+
+    if(aCart.length > 0) {
+        aCart.forEach((product, idProduct) => {
+            const cartContainer = document.createElement("div");
+            cartContainer.className = "productCart";
+            cartContainer.innerHTML = `
+            <img src="${product.img}"/>
+            <p>8 de oro ${product.title}</p>
+            <p>Cantidad: ${product.cant}</p>
+            <p>Precio: $${product.price}</p>
+            <p>Subtotal: $${product.price * product.cant}</p>
+            <button onClick="removeProduct(${idProduct})">Restar</button>
+            `;
+
+            modalCart.appendChild(cartContainer);
+        });
+
+        const totalContainer = document.createElement("div");
+        totalContainer.className = "total-cart";
+        totalContainer.innerHTML = `<div class="total"> TOTAL: $ ${iTotal}</div>
+        <button onClick="buyCart()">FINALIZAR COMPRA</button>`;
+        modalCart.appendChild(totalContainer);
+    } else {
+        modalCart.classList.remove("cart");
+    }
+};
+
+// Array donde almacenamos todo el carrito de compras
+let aCart = [];
+
+// Función en la que agregamos productos al carrito
+const addToCart = (idProduct) => {
+    const idCart = aCart.findIndex((element) => {
+        return element.id === aProducts[idProduct].id;
+    });
+
+    if(idCart === -1) {
+        const addProduct = aProducts[idProduct];
+        addProduct.cant = 1;
+        aCart.push(addProduct);
+        iTotal += aCart[idProduct].price;
+        actStorage(aCart);
+        drawCart();
+    } else {
+        aCart[idCart].cant += 1;
+        iTotal += aCart[idProduct].price;
+        actStorage(aCart);
+        drawCart();
+    }
+};
+
+// Función en la que restamos productos del carrito
+const removeProduct = (idProduct) => {
+    iTotal -= aCart[idProduct].price;
+
+    if(aCart[idProduct].cant <= 1) {
+        aCart.splice(idProduct, 1);
+    } else {
+        aCart[idProduct].cant -= 1;
+    }
+
+    actStorage(aCart);
+    drawCart();
+};
+
+// Función en la que finalizamos la compra y eliminamos el carrito
+const buyCart = () => {
+    alert("¡Gracias por tu compra! El total es: $" + iTotal);
+    iTotal = 0;
+
+    aCart.forEach((product) => {
+        aCart.splice(product);
+    });
+
+    actStorage(aCart);
+    drawCart();
+};
+
+// Actualizamos el almacenamiento
+const actStorage = (cart) => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+};
