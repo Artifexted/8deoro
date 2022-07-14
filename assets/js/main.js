980a96edf0230cd62195dfb524df5ca83e3dad0a
aguslu0 <artifexted@gmail.com>
2022-07-14 10:11:39 -0300

    Optimizacion de JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,32 +6,14 @@
         a = arrays
 */
 
-// Arrays en las cuales almacenamos los productos y precios del sitio
+// Array en la cual almacenamos los productos
 const aProducts = [ 
-    {
-        id: 1,
-        title: "azucaradas",
-        price: 110,
-        img: "./assets/img/azucaradas.jpg",
-        alt: "paquete de 8 de oro azucaradas"
-    },
-    {
-        id: 2,
-        title: "clasicas",
-        price: 80,
-        img: "./assets/img/clasicas.jpg",
-        alt: "paquete de 8 de oro clásicas"
-    },
-    {
-        id: 3,
-        title: "agridulces",
-        price: 90,
-        img: "./assets/img/agridulces.jpg",
-        alt: "paquete de 8 de oro agridulces"
-    }
+    { id: 1, title: "azucaradas",   price: 110, img: "./assets/img/azucaradas.jpg", alt: "paquete de 8 de oro azucaradas" },
+    { id: 2, title: "clasicas",     price: 80,  img: "./assets/img/clasicas.jpg",   alt: "paquete de 8 de oro clásicas" },
+    { id: 3, title: "agridulces",   price: 90,  img: "./assets/img/agridulces.jpg", alt: "paquete de 8 de oro agridulces" }
 ];
 
-// Empezamos a interactuar con HTML
+// Interactuamos con HTML para mostrar la lista de productos
 let htmlCode = ``;
 
 aProducts.forEach((product, idProduct) => {
@@ -47,72 +29,56 @@ const sCards = document.querySelector("#cards");
 
 sCards.innerHTML = htmlCode;
 
-// Comenzamos a crear el carrito
-let modalCart = document.getElementById("cart");
+// Renderizamos el carrito
+let htmlCartID = document.getElementById("cart");
 let iTotal = 0;
 
 const drawCart = () => {
-    modalCart.className = "cart";
-    modalCart.innerHTML = "";
+    htmlCartID.className = "cart";
+    htmlCartID.innerHTML = "";
 
     iTotal = 0;
-
-    loadStorage();
 
     if(aCart.length > 0) {
         aCart.forEach((product, idProduct) => {
             const cartContainer = document.createElement("div");
-            cartContainer.className = "productCart";
-            cartContainer.innerHTML = `
-            <img src="${product.img}"/>
+            cartContainer.innerHTML = `<img src="${product.img}"/>
             <p>8 de oro ${product.title}</p>
             <p>Cantidad: ${product.cant}</p>
             <p>Precio: $${product.price}</p>
             <p>Subtotal: $${product.price * product.cant}</p>
-            <button onClick="removeProduct(${idProduct})">Restar</button>
-            `;
+            <button onClick="removeProduct(${idProduct})">Restar</button>`;
 
             iTotal += product.price * product.cant;
-            modalCart.appendChild(cartContainer);
+            htmlCartID.appendChild(cartContainer);
         });
 
-        const divTotalContainer = document.createElement("div");
-        divTotalContainer.className = "total-cart";
-        divTotalContainer.innerHTML = `<div class="total"> TOTAL: $${iTotal}</div>
+        htmlCartID.innerHTML += `<div> TOTAL: $${iTotal}</div>
         <button onClick="buyCart()">FINALIZAR COMPRA</button>`;
-        modalCart.appendChild(divTotalContainer);
-    } else { modalCart.classList.remove("cart"); }
-};
+    } else { htmlCartID.classList.remove("cart"); }
+}
 
 // Función en la que agregamos productos al carrito
 const addToCart = (idProduct) => {
-    let idCart = aCart.findIndex((element) => {
-        return element.id === aProducts[idProduct].id;
-    });
+    let idCart = aCart.findIndex((element) => { return element.id === aProducts[idProduct].id; });
 
     if(idCart === -1) {
         let addProduct = aProducts[idProduct];
         addProduct.cant = 1;
         aCart.push(addProduct);
-    } else { aCart[idCart].cant += 1; }
+    } else { aCart[idCart].cant++; }
 
     saveStorage(aCart);
-    drawCart();
-};
+}
 
 // Función en la que restamos productos del carrito
 const removeProduct = (idProduct) => {
     iTotal -= aCart[idProduct].price;
 
-    if(aCart[idProduct].cant <= 1) {
-        aCart.splice(idProduct, 1);
-    } else {
-        aCart[idProduct].cant -= 1;
-    }
+    (aCart[idProduct].cant <= 1) ? aCart.splice(idProduct, 1) : aCart[idProduct].cant--;
 
     saveStorage(aCart);
-    drawCart();
-};
+}
 
 // Función en la que finalizamos la compra y eliminamos el carrito
 const buyCart = () => {
@@ -122,29 +88,22 @@ const buyCart = () => {
     aCart.forEach((product) => { aCart.splice(product); });
 
     delStorage();
-    drawCart();
-};
+}
 
 // Guardamos el carrito
-const saveStorage = (cart) => {
-    localStorage.setItem("cart", JSON.stringify(cart));
-};
+const saveStorage = (cart) => { localStorage.setItem("cart", JSON.stringify(cart)); drawCart(); }
 
 // Cargamos el carrito
-const loadStorage = () => {
-    return JSON.parse(localStorage.getItem("cart")) || [];
-}
-
-// Eliminamos el carrito
-const delStorage = () => {
-    localStorage.removeItem("cart");
-}
-
-// Llamamos a la función para cargar el carrito
-loadStorage();
+const loadStorage = () => { return JSON.parse(localStorage.getItem("cart")) || []; }
 
 // Array donde almacenamos todo el carrito de compras
 let aCart = loadStorage();
 
-// Renderizamos/dibujamos el carrito
+// Llamamos a la función para cargar el carrito
+loadStorage();
+
+// Eliminamos el carrito
+const delStorage = () => { localStorage.removeItem("cart"); drawCart(); }
+
+// Llamamos a la función del renderizado del carrito
 drawCart();
